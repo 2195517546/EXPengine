87087fde9a8c215d82204c990d087d0373895f86
Uiloalxise <liumoumoudal@gmail.com>
2023-08-30 17:09:25 +0800

    0.0.2

--- src/main/java/com/expengine/expengine/EXPCommand/EXPenginehelp.java
+++ src/main/java/com/expengine/expengine/EXPCommand/EXPenginehelp.java
@@ -36,10 +36,10 @@ public class EXPenginehelp implements CommandExecutor {
             if(argis.equalsIgnoreCase("exp"))//查询修为
             {
                 String name =sender.getName();//获取命令发送者名字
-                File playerfile = new File(folder.getAbsolutePath()+"\\"+name+".yml");//获取玩家数据路径
-                FileConfiguration fileout = YamlConfiguration.loadConfiguration(playerfile);//输出玩家数据
-                int exppoint = fileout.getInt("exp");
-                sender.sendMessage("经验有"+ exppoint);
+                File playerFile = new File(folder.getAbsolutePath()+"\\"+name+".yml");//获取玩家数据路径
+                FileConfiguration fileout = YamlConfiguration.loadConfiguration(playerFile);//输出玩家数据
+                int expPoint = fileout.getInt("exp");
+                sender.sendMessage("经验有"+ expPoint);
             }
         }else if(args.length==2)
         {
@@ -49,41 +49,46 @@ public class EXPenginehelp implements CommandExecutor {
             {
                 String s_name =sender.getName();
                 String t_name =argis2;//获取目标名称
-                sender.sendMessage(t_name);
 
-
-                Player targetplayer=org.bukkit.Bukkit.getPlayerExact(t_name);
+                Player targetPlayer=org.bukkit.Bukkit.getPlayerExact(t_name);
                 /*
                 * 1.能够taggetplayer.sendMessage(XXXXXX)
                 * 2.targetplayer.getname 必须是 t_name
                 * */
 
-                File targetfile=new File(folder.getAbsolutePath()+"\\"+t_name+".yml");
-                File senderfile=new File(folder.getAbsolutePath()+"\\"+s_name+".yml");
-                if(!targetfile.exists()){
+                File targetFile=new File(folder.getAbsolutePath()+"\\"+t_name+".yml");
+                File senderFile=new File(folder.getAbsolutePath()+"\\"+s_name+".yml");
+
+                if(!targetFile.exists()){
                     sender.sendMessage("此为凡人或不存在");
                     return false;
                 }
-                if (targetplayer == null || !targetplayer.isOnline()) {
+
+                if (targetPlayer == null || !targetPlayer.isOnline()) {
                     sender.sendMessage("此修士暂时脱离了天机之中");
                     return false;
                 }
-                FileConfiguration targetfileout =YamlConfiguration.loadConfiguration(targetfile);//获取目标文件流
-                FileConfiguration senderfileout =YamlConfiguration.loadConfiguration(senderfile);//获取指令方文件流
-                if(targetfileout.getInt("hideexp")==1)
+
+                FileConfiguration targetFileout =YamlConfiguration.loadConfiguration(targetFile);//获取目标文件流
+                FileConfiguration senderFileout =YamlConfiguration.loadConfiguration(senderFile);//获取指令方文件流
+
+                if(targetFileout.getInt("hideexp")==1)
                 {
                     sender.sendMessage("该修士用来秘法遮蔽修为，无法探查。");
-                    targetplayer.sendMessage("有修士探查了你的修为");
+                    targetPlayer.sendMessage("有修士探查了你的修为");
                 }
-                if(senderfileout.getInt("exp")>=targetfileout.getInt("exp") && targetfileout.getInt("hideexp")!=1)
+                if(senderFileout.getInt("exp")>=targetFileout.getInt("exp") && targetFileout.getInt("hideexp")!=1)
                 {
-                    if (senderfileout.getInt("level") == senderfileout.getInt("level")) {
-                        sender.sendMessage("修士" + t_name + "的境界是" + "(一个函数,用于表示境界名称)" + "修为有" + targetfileout.getInt("exp"));
-                        targetplayer.sendMessage("修士" + s_name + "探查了你的修为");
+                    if (senderFileout.getInt("level") == senderFileout.getInt("level")) {
+                        sender.sendMessage("修士" + t_name + "的境界是" + "(一个函数,用于表示境界名称)" + "修为有" + targetFileout.getInt("exp"));
+                        targetPlayer.sendMessage("修士" + s_name + "探查了你的修为");
                     } else {
-                        sender.sendMessage("修士" + t_name + "的境界是" + " (一个函数,用于表示境界名称) " + "修为有" + targetfileout.getInt("exp"));
-                        targetplayer.sendMessage("有修士探查了你的修为");
+                        sender.sendMessage("修士" + t_name + "的境界是" + " (一个函数,用于表示境界名称) " + "修为有" + targetFileout.getInt("exp"));
+                        targetPlayer.sendMessage("你感觉有修士探查了你的修为");
                     }
+                }else {
+                    sender.sendMessage("此人修为高深，无法查探一二");
+                    targetPlayer.sendMessage("你发现修士"+s_name+"尝试探查你的修为");
                 }
             }
         }
